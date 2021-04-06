e647167a1932310ee7d56c66ee613a658c779c76
Jared <jared0x90@gmail.com>
2021-04-06 04:54:13 -0400

    Update colors.asm

--- colors.asm
+++ colors.asm
@@ -1,3 +1,4 @@
+;draws randomly colored pixels around the screen
 start:
 LDA $fe
 LDX $fe
@@ -9,3 +10,18 @@ STA $400, X
 LDX $fe
 STA $500, X
 JMP start
+
+
+; hexdump
+; Address  Hexdump   Dissassembly
+; -------------------------------
+; $0600    a5 fe     LDA $fe
+; $0602    a6 fe     LDX $fe
+; $0604    9d 00 02  STA $0200,X
+; $0607    a6 fe     LDX $fe
+; $0609    9d 00 03  STA $0300,X
+; $060c    a6 fe     LDX $fe
+; $060e    9d 00 04  STA $0400,X
+; $0611    a6 fe     LDX $fe
+; $0613    9d 00 05  STA $0500,X
+; $0616    4c 00 06  JMP $0600
